3be0fad7000b76e7de1d579aadbba378812ceb43
Aoced <aoced33@gmail.com>
2018-06-12 10:20:37 +0200

    Création de la Page d'accueil

---
+++ contact.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>page contact v1</h1>
+
+<form method="post">
+	<input type="text" name="message">
+</form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<h1>page d'accueil</h1>
+Coucou
+</body>
+</html>
